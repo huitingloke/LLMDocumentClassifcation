4488f24523e72212509f93ee7b9a7a110a790d7a
“Huiting <huitingloke@gmail.com>
2025-01-26 20:57:00 +0800

    vdb basic integration

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # FYPDocumentSorter
+
+## Description
+
+KIV
+
+## Setup
+
+* Clone the repository
+* Create a new terminal window and from there, create a new virtual environment with the command `virtualenv env`
+* Activate the environment
+    * Mac: `source env/bin/activate`
+    * Windows: `env\Scripts\activate`
+* Install the requirements with the command `pip install -r requirements.txt`
+* Run the Flask application with `flask --app main run`
+    * You may choose to start it in the debug server by adding the `--debug` flag
+    * If this does not work, use `python3 main.py` while `cd` into the home directory
+
+## Current Schema
+
+* `doc_type`: `str` - Type of document e.g. [ZY to categorize]
+* `doc_id`: `str` -- UUID4 generation of unique document ID
+* `date_uploaded`: `datetime` - Date the document was uploaded
--- main.py
+++ main.py
@@ -1,29 +1,80 @@
-import chromadb
 import datetime
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import pysqlite3 as sqlite3
+from flask import Flask, request
+import uuid
+import chromadb
+from pypdf import PdfReader
+import sys # To view print messages from the same console
 
+app = Flask(__name__)
 
 client = chromadb.PersistentClient(path="./chromadb_persistent_storage/")
 collection = client.get_or_create_collection(
-    name="test", 
-    metadata={
-        "description": "testing",
-        "created": str(datetime.now())
-    }  
+    name="document_storage", 
 )
 
-collection.add(
-    documents=["lorem ipsum...", "lorlorsumsum", "lorsumeeee", ...],
-    metadatas=[{"chapter": "3", "verse": "16"}, {"chapter": "3", "verse": "5"}, {"chapter": "29", "verse": "11"}, ...],
-    ids=["id1", "id2", "id3", ...]
-)
+@app.route("/test", methods=["GET"])
+def test():
+    return "Hello World"
 
-collection.query(
-    query_embeddings=[[11.1, 12.1, 13.1],[1.1, 2.3, 3.2], ...],
-    n_results=10,
-    where={"chapter": "3"},
-    where_document={"$contains":"l"}
-)
+@app.route("/extract_text", methods=["POST"])
+def extract_text(pdf_file:str):
+    reader = PdfReader(pdf_file)
+    for page in reader.pages:
+        text += page.extract
+
+    # USE MODEL HERE TO CLASSIFY TEXT INTO CATEGORIES
+    # WE WILL NOT RETURN TEXT HERE AND WILL INSTEAD LINK TO THE FUNCTION TO STORE TEXT. CURRENTLY, WE WILL RETURN TEXT TO PROVE THE FUNCTION WORKS
+
+    return text
+
+@app.route("/store_documents", methods=["POST"])
+def store_documents():
+    data = request.get_json(force=True) # For testing purposes, not safe for production
+    print(data, file=sys.stderr)
+
+    documents = data.get("documents", None) # Should be a list
+    metadatas = data.get("metadata", None) # Should be a list with objects inside, each object containing metadata
+    
+    if not documents:
+        return False
+
+    # Can submit multiple documents in one shot but prefer not to as we want to do some preprocessing first
+    # Will be slightly slower but overall still more efficient
+    for i in range (len(documents)):
+
+        date = datetime.datetime.now()
+        new_metadata = metadatas[i]
+        new_metadata += {
+            "date": date.timestamp() # Converts to epoch time
+        }
+        
+
+        collection.add(
+        documents=documents[i],
+        metadatas=[new_metadata],
+        ids=[uuid.uuid4()], # Every document requires a unique ID
+    )
+    
+    return collection.count() # Temporary return value to test if the function works
+
+@app.route("/retrieve_documents", methods=["POST"])
+def retrieve_documents():
+    data = request.get_json(force=True) # For testing purposes, not safe for production
+    query = data.get("query", None)
+    if not query:
+        return False
+    
+    results = collection.query(
+        query_texts=[query],
+        n_results=10,
+    )
+
+    # Explanation: Embedding databases have their own algorithms to classify and retrieve documents based on their
+    # proximity in vector space, but we need an LLM to classify the text for its metadata. Technically, you could
+    # just go by the vector space distance, but that may not provide all necessary information.
+    return results
+
+# To run the app: flask --app main run [--debug] OR python[3] main.py
+
+if __name__ == "__main__":
+    app.run(debug=True)
